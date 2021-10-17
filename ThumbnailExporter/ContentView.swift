c9cb882da46bed3f4740c487e7a5c182706c750d
Taha <tsmoujtahid@outlook.de>
2021-10-17 17:18:04 +0200

    Initial Commit

---
+++ ThumbnailExporter/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  ThumbnailExporter
+//
+//  Created by Taha Moujtahid on 17.10.21.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ ThumbnailExporter/ThumbnailExporterApp.swift
@@ -0,0 +1,17 @@
+//
+//  ThumbnailExporterApp.swift
+//  ThumbnailExporter
+//
+//  Created by Taha Moujtahid on 17.10.21.
+//
+
+import SwiftUI
+
+@main
+struct ThumbnailExporterApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
